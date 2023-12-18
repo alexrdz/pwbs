543203148bc724ea9a29958ddac7d6ae1538b211
AlexRdz <drinkhorchata@gmail.com>
2023-12-18 14:16:31 -0500

    update view files

--- views/home.view.php
+++ views/home.view.php
@@ -1,10 +1,2 @@
-<?php ob_start(); ?>
-
-
 <h1 class=""><?= $page->title ?></h1>
 <?= $page->body; ?>
-
-
-<?php
-$content = ob_get_clean();
-include __DIR__ . '/_main.php';
--- views/login.view.php
+++ views/login.view.php
@@ -1,10 +1,2 @@
-<?php ob_start(); ?>
-
-
 <h1 class=""><?= $page->title ?></h1>
 <?php require 'partials/auth.partial.php'; ?>
-
-
-<?php
-$content = ob_get_clean();
-include __DIR__ . '/_main.php';
